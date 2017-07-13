fb84d240ffdc471b9a9fab981e708d4d193fd27b
tday <tday@tyrr.net>
2017-07-13 15:59:18 -0700

    first commit

---
+++ generators/madlib.py
@@ -0,0 +1,39 @@
+import json
+import sys
+from random import choice
+"""
+
+    madlibbing engine
+
+"""
+
+
+class Grammar(object):
+
+    def __init__(self, rules):
+        self.rules = rules
+
+    def flatten(self, rule_name):
+        genned_text = []
+        for k, v in self.rules[rule_name].items():
+            if k == "pattern":
+                for item in v:
+                    if item.startswith("#"):
+                        genned_text.append(self.flatten(item[1:]))
+                    else:
+                        genned_text.append(item)
+            if k == "bucket":
+                selection = choice(v)
+                if selection.startswith("#"):
+                    genned_text.append(self.flatten(selection[1:]))
+                else:
+                    genned_text.append(selection)
+        return " ".join(genned_text)
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    with open(filename) as fn:
+        rules = json.load(fn)
+    g = Grammar(rules)
+    print(g.flatten("main"))
